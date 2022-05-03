1a3c906f69c586d6a164c4bd4ef619cd61db1e8e
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-03 14:06:59 -0400

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Enter an integer: ");
+var answer = Console.ReadLine();
+var theNbr = Convert.ToInt32(answer);
+
+while (theNbr != 0) {
+    if (theNbr % 2 == 0) {
+        Console.WriteLine($"The number {theNbr} is even");
+    } else { 
+        Console.WriteLine($"The number {theNbr} is odd");
+}
+
+    Console.Write("Enter an integer: ");
+    answer = Console.ReadLine();
+    theNbr = Convert.ToInt32(answer);
+}
